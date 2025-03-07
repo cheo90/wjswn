7387d7c8248f9fe462997830e21f6f3dcfcbff04
kim myeong jin <gimm07584@gmail.com>
2025-03-07 14:57:35 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    const MAINVISUALSLIDE = new Swiper('.main_visual_slide', {
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        effect: "creative",
+        creativeEffect: {
+            prev: {
+                shadow: true,
+                translate: ["-20%", 0, -1],
+            },
+            next: {
+                translate: ["100%", 0, 0],
+            },
+        },
+
+        navigation: {
+            nextEl: '.main_visual .arrows .next',
+            prevEl: '.main_visual .arrows .prev',
+        },
+
+    });
+
+
+});
+
+$(function () {
+    const MMS = new Swiper('.main_menu_slide', {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        speed: 8000,
+        loop: true,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+    });
+
+});
+
+
+$(function () {
+    $('.main_content .best_tab_menu button').on('click', function () {
+        let idx = $(this).parent().index();
+        $('.main_content .best_tab_menu button').removeClass('on');
+        $(this).addClass('on');
+        $('.main_content .best_tab_content .con').removeClass('on');
+        $('.main_content .best_tab_content .con').eq(idx).addClass('on')
+    })
+});
+
